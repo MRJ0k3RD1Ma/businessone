84c76789bf5cb255995850d3e70f7a6eafb4e92f
Dilmurod <mr.j0k3r.d1ma@gmail.com>
2023-09-10 17:33:49 +0500

    sdf

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -10,14 +10,44 @@ use yii\web\AssetBundle;
 class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $baseUrl = '@web/code';
     public $css = [
-        'css/site.css',
+//        'css/bootstrap.css',
+        'css/settings.css',
+        'css/layers.css',
+        'css/navigation.css',
+        'css/style.css',
+        'css/responsive.css',
     ];
     public $js = [
+//        'js/jquery.js',
+        'js/popover.js',
+        'js/bootstrap.min.js',
+        'js/jquery.themepunch.revolution.min.js',
+        'js/jquery.themepunch.tools.min.js',
+        'js/revolution.extension.actions.min.js',
+        'js/revolution.extension.carousel.min.js',
+        'js/revolution.extension.kenburn.min.js',
+        'js/revolution.extension.layeranimation.min.js',
+        'js/revolution.extension.migration.min.js',
+        'js/revolution.extension.navigation.min.js',
+        'js/revolution.extension.parallax.min.js',
+        'js/revolution.extension.slideanims.min.js',
+        'js/revolution.extension.video.min.js',
+        'js/main-slider-script.js',
+
+        'js/plugins.js',
+        'js/validate.js',
+        'js/jquery.fancybox.pack.js',
+        'js/jquery.fancybox-media.js',
+
+        'js/script.js',
+
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap5\BootstrapAsset',
     ];
 }
+?>
+
